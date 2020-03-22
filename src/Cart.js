f6bbd156d1e70929e9b3d6bf3dca8707b06c8d77
pratik-dutta <pratikdutta.786@gmail.com>
2020-03-22 17:17:29 +0530

    add dynamic count and move state from cart to app

--- src/Cart.js
+++ src/Cart.js
@@ -2,87 +2,25 @@ import React from "react";
 import CartItem from "./CartItem";
 
 
-class Cart extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            products: [
-                {
-                    price: 999,
-                    title: "Phone",
-                    qty: 1,
-                    img: "",
-                    id:1
-                },
-                {
-                    price: 999,
-                    title: "Watch",
-                    qty: 1,
-                    img: "",
-                    id:2
-                },
-                {
-                    price: 999,
-                    title: "Laptop",
-                    qty: 1,
-                    img: "",
-                    id:3
-                }
-            ]
-        }
-        // this.increaseQty = this.increaseQty.bind(this);
-    }
-    handleIncreaseQty = (product) => {
-        console.log(product);
-        const { products } = this.state;
-        const index = products.indexOf(product);
-        products[index].qty += 1;
-        this.setState({
-            products 
-            //or
-            //products = products
-        });
-    }
-    handleDecreaseQty = (product) => {
-        console.log(product);
-        const { products } = this.state;
-        const index = products.indexOf(product);
-        if(products[index].qty === 0){
-            return;
-        }
-        products[index].qty -= 1;
-        this.setState({
-            products 
-            //or
-            //products = products
-        });
-    }
-    handleDelete = (id) => {
-        const { products } = this.state;
-        const items = products.filter((item) => item.id !== id);
-        
-        this.setState({
-            products : items
-        });
-    }
-    render() {
-        const { products } = this.state;
-        return (
-            <div className="cart">
-                {products.map((product) => {
-                    return (
-                        <CartItem 
-                            product = {product} 
-                            key={product.id}
-                            onIncreaseQty = {this.handleIncreaseQty}
-                            onDecreaseQty = {this.handleDecreaseQty}
-                            onDelete = {this.handleDelete}
-                        />
-                    )
-                })}
-            </div>
-        );
-    }
+const Cart = (props) => {
+    
+    const { products } = props;
+    return (
+        <div className="cart">
+            {products.map((product) => {
+                return (
+                    <CartItem 
+                        product = {product} 
+                        key={product.id}
+                        onIncreaseQty = {props.onIncreaseQty}
+                        onDecreaseQty = {props.onDecreaseQty}
+                        onDelete = {props.onDelete}
+                    />
+                );
+            })}
+        </div>
+    );
+    
 }
 
 
