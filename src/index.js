26c78dde02bf6d5c5853c21d6702f8ccbfcff2c2
Pratik Dutta <pratikdutta.786@gmail.com>
2020-04-02 02:21:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAbbRUI_AEXLAD5xH_VEyE5_pHtRjl6-24",
-    authDomain: "cart-4b40e.firebaseapp.com",
+    apiKey: "FIREBASE_API_KEY",
+    authDomain: "FURBASE_AUTHDOMAIN",
     databaseURL: "https://cart-4b40e.firebaseio.com",
     projectId: "cart-4b40e",
     storageBucket: "cart-4b40e.appspot.com",
-    messagingSenderId: "671972633552",
-    appId: "1:671972633552:web:a9c106c1d72308369a27d5"
+    messagingSenderId: "FIREBASE",
+    appId: "FIREBASE_ID"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
