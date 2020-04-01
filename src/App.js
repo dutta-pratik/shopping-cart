62a9b2da5d0acbc7700dc015a57929086817ad7b
pratik-dutta <pratikdutta.786@gmail.com>
2020-04-02 01:32:35 +0530

    update product in firebase

--- src/App.js
+++ src/App.js
@@ -45,11 +45,22 @@ class App extends React.Component {
     const { products } = this.state;
     const index = products.indexOf(product);
 
-    products[index].qty += 1;
+    // products[index].qty += 1;
 
-    this.setState({
-      products
-    });
+    // this.setState({
+    //   products
+    // });
+
+    const docRef = this.db.collection("products").doc(products[index].id);
+
+    docRef
+      .update({ qty: products[index].qty + 1 })
+      .then(() => {
+        console.log("Document updated sucessfully");
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   handleDecreaseQuantity = product => {
